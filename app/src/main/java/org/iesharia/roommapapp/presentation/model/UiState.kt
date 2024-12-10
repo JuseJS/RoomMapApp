ac5a69fadf197f4347423822bc69d25aa3ca89b5
Navarro <joseanp0202@gmail.com>
2024-12-10 16:04:50 +0000

    feat: Creado UiState base para gestionar estados comunes en la UI

---
+++ app/src/main/java/org/iesharia/roommapapp/presentation/model/UiState.kt
@@ -0,0 +1,7 @@
+package org.iesharia.roommapapp.presentation.model
+
+sealed interface UiState<out T> {
+    object Loading : UiState<Nothing>
+    data class Success<T>(val data: T) : UiState<T>
+    data class Error(val message: String) : UiState<Nothing>
+}
