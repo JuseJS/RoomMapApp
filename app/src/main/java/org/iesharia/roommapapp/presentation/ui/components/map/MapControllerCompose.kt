bca8bdf330280ecc855cdf540e9c94ca9a157069
Navarro <joseanp0202@gmail.com>
2024-12-10 22:00:11 +0000

    refactor: streamline MapControllerCompose for better state management

--- app/src/main/java/org/iesharia/roommapapp/presentation/ui/components/map/MapControllerCompose.kt
+++ app/src/main/java/org/iesharia/roommapapp/presentation/ui/components/map/MapControllerCompose.kt
@@ -12,15 +12,24 @@ import androidx.core.content.ContextCompat
 import org.iesharia.roommapapp.R
 import org.iesharia.roommapapp.domain.model.MapIconType
 import org.iesharia.roommapapp.domain.model.MarkerModel
+import org.osmdroid.tileprovider.MapTileProviderBasic
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.TilesOverlay
 
 class MapControllerCompose(
     private val context: Context,
     private val mapView: MapView
 ) {
     private var selectedMarker: MarkerModel? by mutableStateOf(null)
+    private val tileProvider = MapTileProviderBasic(context)
+
+    init {
+        // Configurar el tile provider
+        mapView.tileProvider = tileProvider
+        mapView.overlayManager.tilesOverlay = TilesOverlay(tileProvider, context)
+    }
 
     private fun getMarkerDrawable(typeId: Long): Drawable? {
         val iconType = MapIconType.fromTypeId(typeId)
@@ -51,10 +60,15 @@ class MapControllerCompose(
             setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
 
             // Establecer el icono correspondiente al tipo de marcador
-            icon = getMarkerDrawable(marker.type.id)
-
-            // Opcional: Mostrar el título encima del marcador
-            setTextIcon(title)
+            icon = getMarkerDrawable(marker.type.id)?.apply {
+                val scaleFactor = 1.5f
+                setBounds(
+                    0,
+                    0,
+                    (intrinsicWidth * scaleFactor).toInt(),
+                    (intrinsicHeight * scaleFactor).toInt()
+                )
+            }
 
             // Configurar comportamiento al seleccionar el marcador
             setOnMarkerClickListener { _, _ ->
@@ -86,7 +100,6 @@ class MapControllerCompose(
             Log.d("MapController", "Marcadores obtenidos: ${markers.size}")
         } catch (e: Exception) {
             Log.e("MapController", "Error al actualizar los marcadores: ", e)
-            // Implementar un callback para notificar errores al ViewModel
         }
     }
 
@@ -101,10 +114,5 @@ class MapControllerCompose(
                 onDismiss = { selectedMarker = null }
             )
         }
-    }
-
-    private fun Marker.setTextIcon(text: String) {
-        // Configuración para mostrar texto encima del marcador, si es necesario
-        // Se podría implementar con un TextDrawable o una vista personalizada
     }
 }
