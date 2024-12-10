a6c5137d23f1a3adc4707a80f4e85e8f1abcf3ca
Navarro <joseanp0202@gmail.com>
2024-12-10 22:03:31 +0000

    refactor: adapt MarkerTypeRepository for new marker type management

--- app/src/main/java/org/iesharia/roommapapp/domain/repository/MarkerTypeRepository.kt
+++ app/src/main/java/org/iesharia/roommapapp/domain/repository/MarkerTypeRepository.kt
@@ -2,11 +2,12 @@ package org.iesharia.roommapapp.domain.repository
 
 import kotlinx.coroutines.flow.Flow
 import org.iesharia.roommapapp.domain.model.MarkerTypeModel
+import org.iesharia.roommapapp.domain.util.Result
 
 interface MarkerTypeRepository {
-    fun getAllMarkerTypes(): Flow<List<MarkerTypeModel>>
-    suspend fun getMarkerTypeById(typeId: Long): MarkerTypeModel?
-    suspend fun addMarkerType(markerType: MarkerTypeModel)
-    suspend fun updateMarkerType(markerType: MarkerTypeModel)
-    suspend fun deleteMarkerType(typeId: Long)
+    fun getAllMarkerTypes(): Flow<Result<List<MarkerTypeModel>>>
+    suspend fun getMarkerTypeById(typeId: Long): Result<MarkerTypeModel>
+    suspend fun addMarkerType(markerType: MarkerTypeModel): Result<Unit>
+    suspend fun updateMarkerType(markerType: MarkerTypeModel): Result<Unit>
+    suspend fun deleteMarkerType(typeId: Long): Result<Unit>
 }
