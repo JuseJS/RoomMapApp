c4edf4b2c811aa1a36a0398f5aa2fce4ccb75c13
Navarro <joseanp0202@gmail.com>
2024-12-10 22:02:59 +0000

    refactor: update CustomMapRepository to align with new mapper structure

--- app/src/main/java/org/iesharia/roommapapp/domain/repository/CustomMapRepository.kt
+++ app/src/main/java/org/iesharia/roommapapp/domain/repository/CustomMapRepository.kt
@@ -2,13 +2,14 @@ package org.iesharia.roommapapp.domain.repository
 
 import kotlinx.coroutines.flow.Flow
 import org.iesharia.roommapapp.domain.model.CustomMapModel
+import org.iesharia.roommapapp.domain.util.Result
 
 interface CustomMapRepository {
-    fun getAllCustomMaps(): Flow<List<CustomMapModel>>
-    suspend fun getDefaultMap(): CustomMapModel?
-    suspend fun getCustomMapById(mapId: Long): CustomMapModel?
-    suspend fun addCustomMap(customMap: CustomMapModel)
-    suspend fun updateCustomMap(customMap: CustomMapModel)
-    suspend fun deleteCustomMap(mapId: Long)
-    suspend fun setDefaultMap(mapId: Long)
+    fun getAllCustomMaps(): Flow<Result<List<CustomMapModel>>>
+    suspend fun getDefaultMap(): Result<CustomMapModel>
+    suspend fun getCustomMapById(mapId: Long): Result<CustomMapModel>
+    suspend fun addCustomMap(customMap: CustomMapModel): Result<Unit>
+    suspend fun updateCustomMap(customMap: CustomMapModel): Result<Unit>
+    suspend fun deleteCustomMap(mapId: Long): Result<Unit>
+    suspend fun setDefaultMap(mapId: Long): Result<Unit>
 }
